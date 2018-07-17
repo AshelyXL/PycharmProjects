65cf776c0b01ed7ad209685a048f2ec6a39b6d63
AshelyXL <1002839742@qq.com>
2018-07-17 14:47:57 +0800

    delete some backspaces

--- venv/HelloWorld.py
+++ venv/HelloWorld.py
@@ -1,4 +1,3 @@
-# -*- coding: UTF-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,8 +14,4 @@ plt.show()
 
 
 
-print('\n'.join([''.join([('LoveDaLin'[(x-y)%8]
-                           if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0
-                           else' ')
-                          for x in range(-30,30)])
-                 for y in range(15,-15,-1)]))
+print('\n'.join([''.join([('LoveDaLin'[(x-y)%8]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0else' ')for x in range(-30,30)])for y in range(15,-15,-1)]))
